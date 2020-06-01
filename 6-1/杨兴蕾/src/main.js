5b217580f511ceae696a5852a97b182124268d18
YOYOyxl <2256678514@qq.com>
2020-06-01 09:10:40 +0800

    杨兴蕾

---
+++ 6-1/杨兴蕾/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Axios from 'axios'
+import less from 'less'
+import initRichText from './common/initHTMLEditor.js'
+import 'font-awesome/css/font-awesome.min.css'
+//   引入echart插件
+import echarts from 'echarts'
+Vue.prototype.$echarts = echarts
+Vue.config.productionTip = false
+Vue.prototype.$axios = Axios
+Vue.use(ElementUI)
+Vue.use(less)
+Vue.use(initRichText)
+new Vue({
+  router: router,
+  render: h => h(App)
+}).$mount('#app')
